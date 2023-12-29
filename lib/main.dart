5c1825d9e33185bef742d4b140778997f738aa0c
Hwang gend <ghkdtjsdks@naver.com>
2023-12-29 22:41:50 +0900

    Chapter.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//위젯들을 레고블럭 처럼 합쳐서 앱을 만들어냄
+//위젯들을 다 외울 필요는 없음
 void main() {
-  runApp(MyApp());
+  //처음 시작되는 App이라는 위젯이 root이다.
+  //앱의 root는 material or IOS 디자인을 사용할지결정을 해야한다.
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+//class의 생성자 같이 statelessWidget을 상속바으면
+//build라는 메소드를 구현해줘야한다.
+//build 메소드가 ui를 생성하게 됨(build return값을 화면에 출력)
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //return으로 material(구글), cupertino(애플)디자인 시스템
+    //둘 중 하나를 return 하게됨
+    //scaffold를 통해 구조를 잡아줘야 한다. 레이아웃 개념인듯
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Hey, Selena',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.8),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              )
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('Hello Flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
     );
+    //보통 materialApp이 flutter도 구글이 만들었기에
+    //더 보기 좋다.
   }
 }
